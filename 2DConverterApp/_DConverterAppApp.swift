11e4abdbc80c262d730bd0bae77b3db384c01c9c
YohanesVitoMBP <yohanes.vito@ti.ukdw.ac.id>
2026-08-10 08:45:22 +0800

    first commit

---
+++ 2DConverterApp/_DConverterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  _DConverterAppApp.swift
+//  2DConverterApp
+//
+//  Created by Yohanes Vito Rizki D on 10/08/26.
+//
+
+import SwiftUI
+
+@main
+struct _DConverterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
